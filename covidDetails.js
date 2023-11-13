04f10a7d5bec219604616282b3ac303b859baf7d
Vicky <vkrngn@gmail.com>
2023-11-13 12:57:20 +0530

    Got covid data from web.

--- covidDetails.js
+++ covidDetails.js
@@ -1,13 +1,30 @@
 const request = require("request");
 // cheerio - parses HTML and traverse it so that data can be manupulated according to user's need
+// cheerio stores data in the form of objects
 const cheerio = require("cheerio");
 
 request("https://www.worldometers.info/coronavirus/", cb);
 
 function cb(err, res, body) {
-  console.log("error ", err);
-  // console.log(res);
-  console.log(body);
+  if (err) {
+    console.error("error ", err);
+  } else {
+    handleHtml(body);
+  }
+}
 
-  // Extracting data from HTML
+function handleHtml(html) {
+  let selectTool = cheerio.load(html);
+
+  let coronaStats = selectTool(".maincounter-number");
+
+  //   console.log(coronaStats.text());
+  let totalCases = selectTool(coronaStats[0]).text();
+  console.log(`Total Cases -> ${totalCases}`);
+
+  let totalDeath = selectTool(coronaStats[1]).text();
+  console.log(`Total Deaths -> ${totalDeath}`);
+
+  let totalRecovered = selectTool(coronaStats[2]).text();
+  console.log(`Total Recovered -> ${totalRecovered}`);
 }
